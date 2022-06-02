d59bc4a249c4277b9d51e281594b3edbf3d2ae70
kayjinyi <95940771+kayjinyi@users.noreply.github.com>
2022-06-02 12:20:15 -0700

    css

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -7,19 +7,19 @@ function NavTabs() {
       <h1>YI JIN</h1>
       <nav>
         <ul>
-          <li>
+          <li className="nav-item">
             <Link to="/">About Me</Link>
           </li>
 
-          <li>
+          <li className="nav-item">
             <Link to="/portfolio">Portfolio</Link>
           </li>
 
-          <li>
+          <li className="nav-item">
             <Link to="/contact">Contact</Link>
           </li>
 
-          <li>
+          <li className="nav-item">
             <Link to="/resume">Resume</Link>
           </li>
         </ul>
